03f0756eddd0136f9f01be9ceb609589693eecda
Léo Peyronnet <peyronnet.leo@gmail.com>
2025-07-16 10:49:35 +0200

    feat: GestionnaireBorne

--- js/bornes.js
+++ js/bornes.js
@@ -5,8 +5,16 @@ class Borne {
         this.lon = lon;
     }
 
+    // Méthode utilitaire pour valider les coordonnées
+    isValidCoordinates() {
+        const latValid = this.lat >= -90 && this.lat <= 90;
+        const lonValid = this.lon >= -180 && this.lon <= 180;
+        return latValid && lonValid;
+    }
+
+    // Méthode de rendu HTML
     toHTML() {
-        return "<li>Erreur : Borne factice</li>"
+        return "<li>Borne factice</li>"
     }
 }
 
@@ -17,7 +25,11 @@ class BornePublique extends Borne {
 
     toHTML() {
         const li = document.createElement("li");
-        li.textContent = `Borne n°${this.id} - latitude : ${this.lat}, longitude : ${this.lon}`;
+        if (this.isValidCoordinates()) {
+            li.textContent = `Borne ${this.id} - latitude : ${this.lat}, longitude : ${this.lon}`;
+        } else {
+            li.textContent = "Borne avec coordonnées invalides !"
+        }
         return li.outerHTML;
     }
 }
@@ -30,8 +42,38 @@ class BornePrivee extends Borne {
 
     toHTML() {
         const li = document.createElement("li");
-        li.textContent = `Borne n°${this.id} de ${this.owner} - latitude : ${this.lat}, longitude : ${this.lon}`;
+        if (this.isValidCoordinates()) {
+            li.textContent = `Borne ${this.id} de ${this.owner} - latitude : ${this.lat}, longitude : ${this.lon}`;
+        } else {
+            li.textContent = "Borne avec coordonnées invalides !"
+        }
         return li.outerHTML;
     }
 }
 
+class GestionnaireBorne {
+    constructor() {
+        this.gestionnaire = [];
+    }
+
+    ajouterBorne(borne) {
+        if (borne instanceof Borne) {
+            this.bornes.push(borne);
+        } else {
+            throw new Error('Objet doit être une instance de Borne');
+        }
+    }
+
+    toHTML() {
+        const ul = document.createElement('ul');
+        ul.className = 'liste-bornes';
+        
+        this.bornes.forEach(borne => {
+            ul.innerHTML += borne.toHTML();
+        });
+        
+        return ul.outerHTML;
+    }
+}
+
+export {Borne, BornePublique, BornePrivee, GestionnaireBorne}
